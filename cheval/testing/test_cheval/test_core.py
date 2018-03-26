49cbbdf7ec9ee15196a84cc940d4b88a1ca921a6
PeterKucirek <peter.kucirek@gmail.com>
2018-03-25 20:33:44 -0400

    Fixed errors and added logsum test for multinomial logit function

--- cheval/testing/test_cheval/test_core.py
+++ cheval/testing/test_cheval/test_core.py
@@ -24,32 +24,36 @@ class TestChevalCore(unittest.TestCase):
         expected_result = np.float64(
             [0.181775432, 0.183785999, 0.130682672, 0.083744629, 0.214813892, 0.08159533, 0.068632901, 0.054969145]
         )
-        test_result = multinomial_probabilities(utilities)
+        expected_ls = 29.4585222
 
-        assert_allclose(test_result, expected_result)
+        test_result, test_ls = multinomial_probabilities(utilities)
+
+        assert_allclose(test_result, expected_result, rtol=0.000001)
+        assert abs(expected_ls - test_ls) < 0.000001
 
     def _build_nested_tree(self):
         tree = ChoiceTree()
-        a = tree.add('a', 0.3)
-        b = tree.add('b', 0.6)
 
-        a.add('c')
-        a.add('d')
+        auto = tree.add('auto', logsum_scale=0.7)
+        auto.add('carpool')
+        auto.add('drive')
 
-        b.add('e')
-        f = b.add('f', 0.5)
+        transit = tree.add('transit', logsum_scale=0.7)
+        transit.add('bus')
+        train = transit.add('train', logsum_scale=0.3)
 
-        f.add('g')
-        f.add('h')
+        train.add('drive')
+        train.add('walk')
 
         return tree.flatten()
 
     def test_nested_logit(self):
-        utilities = self._get_utility_row()
+        utilities = np.float64([-0.001, -1.5, -0.5, -0.005, -1, -0.075, -0.3, -0.9])
         tree_info = self._build_nested_tree()
         test_result = nested_probabilities(utilities, tree_info)
 
-        expected_result = np.float64([0, 0.06527, 0.17893, 0.47448, 0.23791, 0.04341, 0, 0])
+        # Auto, Auto-carpool, auto-drive, transit, transit-bus, transit-train, train-bus, train-walk
+        expected_result = np.float64([0, 0.0852, 0.3555, 0, 0.1563, 0, 0.3549, 0.0480])
 
         assert np.allclose(expected_result, test_result)
 
