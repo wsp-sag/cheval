cf9d978ed3eb03daa821c7c5c7cf3670ee592897
PeterKucirek <peter.kucirek@gmail.com>
2018-04-14 22:54:03 -0400

    Reorganized core tests

--- cheval/testing/test_core.py
+++ cheval/testing/test_core.py
@@ -4,62 +4,16 @@ from bisect import bisect_right
 import numpy as np
 from numpy.testing import assert_allclose
 
-from core import (sample_once, sample_multi, logarithmic_search, multinomial_probabilities, nested_probabilities,
-                  simple_probabilities, MIN_RANDOM_VALUE, simple_sample, simple_multisample,
-                  worker_multinomial_sample, worker_nested_sample)
+from core import (
+    MIN_RANDOM_VALUE, sample_once, sample_multi, logarithmic_search,
+    simple_probabilities, simple_sample, simple_multisample, worker_weighted_sample,
+    multinomial_probabilities, multinomial_sample, multinomial_multisample, worker_multinomial_sample, worker_multinomial_probabilities,
+    nested_probabilities, nested_sample, nested_multisample, worker_nested_sample, worker_nested_probabilities
+)
 from tree import ChoiceTree
 
 
-class TestChevalCore(unittest.TestCase):
-
-    def _get_utility_row(self):
-        ret = np.array(
-            [1.678, 1.689, 1.348, 0.903, 1.845, 0.877, 0.704, 0.482],
-            dtype=np.float64
-        )
-
-        return ret
-
-    def test_mulitnomial_logit(self):
-        utilities = self._get_utility_row()
-
-        expected_result = np.float64(
-            [0.181775432, 0.183785999, 0.130682672, 0.083744629, 0.214813892, 0.08159533, 0.068632901, 0.054969145]
-        )
-        expected_ls = 29.4585222
-
-        test_result, test_ls = multinomial_probabilities(utilities)
-
-        assert_allclose(test_result, expected_result, rtol=0.000001)
-        assert abs(expected_ls - test_ls) < 0.000001
-
-    def _build_nested_tree(self):
-        tree = ChoiceTree()
-
-        auto = tree.add('auto', logsum_scale=0.7)
-        auto.add('carpool')
-        auto.add('drive')
-
-        transit = tree.add('transit', logsum_scale=0.7)
-        transit.add('bus')
-        train = transit.add('train', logsum_scale=0.3)
-
-        train.add('drive')
-        train.add('walk')
-
-        return tree.flatten()
-
-    def test_nested_logit(self):
-        utilities = np.float64([-0.001, -1.5, -0.5, -0.005, -1, -0.075, -0.3, -0.9])
-        tree_info = self._build_nested_tree()
-        test_result, test_ls = nested_probabilities(utilities, *tree_info)
-
-        # Auto, Auto-carpool, auto-drive, transit, transit-bus, transit-train, train-bus, train-walk
-        expected_result = np.float64([0, 0.085207, 0.355547, 0, 0.156274, 0, 0.354937, 0.048035])
-        expected_ls = 1.597835
-
-        assert_allclose(test_result, expected_result, rtol=0.00001)
-        assert abs(expected_ls - test_ls) < 0.000001
+class TestSharedCore(unittest.TestCase):
 
     def test_sample_once(self):
         p = np.float64([0, 0, 0.25, 0.00, 0.25, 0.15, 0.35, 0.0])
@@ -114,8 +68,11 @@ class TestChevalCore(unittest.TestCase):
             standard_result = bisect_right(cumsums, random_draw)
             assert test_result == standard_result
 
+
+class TestWeighredSampling(unittest.TestCase):
+
     def test_simple_probabilities(self):
-        weights = self._get_utility_row()
+        weights = np.float64([1.678, 1.689, 1.348, 0.903, 1.845, 0.877, 0.704, 0.482])
 
         expected_result = weights / weights.sum()
 
@@ -139,21 +96,74 @@ class TestChevalCore(unittest.TestCase):
             test_result = simple_sample(weights, r)
             assert expected_index == test_result, f"Test={i} Expected={expected_index} Actual={test_result}"
 
-    def test_simple_multisample(self):
+
+class TestMultinomialCore(unittest.TestCase):
+
+    def test_probabilities(self):
+        utilities = np.float64([1.678, 1.689, 1.348, 0.903, 1.845, 0.877, 0.704, 0.482])
+
+        expected_result = np.float64(
+            [0.181775432, 0.183785999, 0.130682672, 0.083744629, 0.214813892, 0.08159533, 0.068632901, 0.054969145]
+        )
+        expected_ls = 29.4585222
+
+        test_result, test_ls = multinomial_probabilities(utilities)
+
+        assert_allclose(test_result, expected_result, rtol=0.000001)
+        assert abs(expected_ls - test_ls) < 0.000001
+
+    def test_sample_once(self):
         pass
 
-    def test_multinomial_sample(self):
+    def test_multisample(self):
         pass
 
-    def test_multinomial_multisample(self):
+    def test_worker_sampling(self):
         pass
 
-    def test_nested_sample(self):
+    def test_worker_probabilities(self):
         pass
 
-    def test_nested_multisample(self):
+
+class TestChevalCore(unittest.TestCase):
+
+    def _build_nested_tree(self):
+        tree = ChoiceTree()
+
+        auto = tree.add('auto', logsum_scale=0.7)
+        auto.add('carpool')
+        auto.add('drive')
+
+        transit = tree.add('transit', logsum_scale=0.7)
+        transit.add('bus')
+        train = transit.add('train', logsum_scale=0.3)
+
+        train.add('drive')
+        train.add('walk')
+
+        return tree.flatten()
+
+    def test_probabilities(self):
+        utilities = np.float64([-0.001, -1.5, -0.5, -0.005, -1, -0.075, -0.3, -0.9])
+        tree_info = self._build_nested_tree()
+        test_result, test_ls = nested_probabilities(utilities, *tree_info)
+
+        # Auto, Auto-carpool, auto-drive, transit, transit-bus, transit-train, train-bus, train-walk
+        expected_result = np.float64([0, 0.085207, 0.355547, 0, 0.156274, 0, 0.354937, 0.048035])
+        expected_ls = 1.597835
+
+        assert_allclose(test_result, expected_result, rtol=0.00001)
+        assert abs(expected_ls - test_ls) < 0.000001
+
+    def test_sample_once(self):
         pass
 
-    def test_multithreaded_stability(self):
+    def test_multisample(self):
+        pass
+
+    def test_worker_sampling(self):
+        pass
+
+    def test_worker_probabilities(self):
         pass
 
