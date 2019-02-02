b1fa00c0aae152bfae09356ef7dfd23e9e28babb
PeterKucirek <peter.kucirek@gmail.com>
2019-02-02 11:58:45 -0500

    Moved convert_series to its own file, to avoid circular referencing

--- cheval/api.py
+++ cheval/api.py
@@ -11,6 +11,7 @@ from .parsing.expr_items import ChainTuple, ChainedSymbol
 from .parsing.expressions import Expression
 from .ldf import LinkedDataFrame
 from .exceptions import ModelNotReadyError
+from .misc import convert_series
 if TYPE_CHECKING:
     from .model import ChoiceModel
 
@@ -121,39 +122,6 @@ class ExpressionGroup(object):
 
     def __iter__(self) -> Generator[Expression, None, None]:
         yield from self._expressions
-
-
-def convert_series(s: pd.Series, allow_raw=False) -> np.ndarray:
-    dtype = s.dtype
-
-    if dtype.name == 'category':
-        # Categorical column
-        categorical = s.values
-
-        category_index = categorical.categories
-        if category_index.dtype.name == 'object':
-            max_len = category_index.str.len().max()
-            typename = 'S%s' % max_len
-        else:
-            typename = category_index.dtype
-
-        return categorical.astype(typename)
-    elif dtype.name == 'object':
-        # Object or text column
-        max_length = s.str.len().max()
-        if np.isnan(max_length):
-            raise TypeError("Could not get max string length")
-
-        return s.values.astype("S%s" % max_length)
-    elif np.issubdtype(dtype, np.datetime64):
-        raise TypeError("Datetime columns are not supported")
-    elif np.issubdtype(dtype, np.timedelta64):
-        raise TypeError("Timedelta columns are not supported")
-    try:
-        return s.values[...]
-    except:
-        if allow_raw: return s
-        raise
 
 
 class AbstractSymbol(object, metaclass=abc.ABCMeta):
