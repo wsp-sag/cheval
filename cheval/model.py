168493b5c3d9cd20e10d876e9e2f410be4d3cf68
PeterKucirek <pkucirek@pbworld.com>
2019-05-14 16:56:59 -0400

    Removed dedicated utility eval

--- cheval/model.py
+++ cheval/model.py
@@ -428,29 +428,4 @@ class ChoiceModel(object):
 
         return result_frame, logsum
 
-    def evaluate_utilities_only(self, n_threads: int=1, clear_scope: bool=True, precision: int=8,
-                                logger: Logger=None) -> DataFrame:
-        """
-        Do not run the full model, but instead just compute the utility table and return it. This can be useful when
-        building complex nested models; and for when intermediate indexing of the utilieis is required.
-
-        Args:
-            n_threads: The number of threads to uses in the computation. Must be >= 1
-            clear_scope: If True and override_utilities not provided, data stored in the scope for
-                utility computation will be released, freeing up memory. Turning this off is of limited use.
-            precision: The number of bytes to store for each cell in the utility array; one of 1, 2, 4, or 8. More
-                precision requires more memory.
-
-        Returns:
-            DataFrame: Utilities for each choice for each decision unit.
-
-        """
-        self.validate_tree()
-        self.validate_scope()
-
-        utility_table = self._evaluate_utilities(precision=precision, n_threads=n_threads, logger=logger)
-        if clear_scope: self.clear_scope()
-
-        return utility_table
-
     # endregion
